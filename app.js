61536d9b4735223a404a648141aeaed4a48d5db6
Ishan <ishansrivastava123@gmail.com>
2023-05-18 00:21:23 +0100

    random changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ config({
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     headers: ["Content-Type", "X-Auth-Token", "Origin", "Authorization"],
     credentials: true
