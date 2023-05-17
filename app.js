429ac6752dc374f481dba2101a706d431296164d
Ishan <ishansrivastava123@gmail.com>
2023-05-17 23:58:30 +0100

    random changes

--- app.js
+++ app.js
@@ -16,12 +16,19 @@ config({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    headers: ["Content-Type", "X-Auth-Token", "Origin", "Authorization"],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: [process.env.FRONTEND_URL],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     headers: ["Content-Type", "X-Auth-Token", "Origin", "Authorization"],
+//     credentials: true
+// }));
+
+app.use(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+});
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/task", taskRouter);
