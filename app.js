86e286a85d8ca52ee2dd8b3827813f9c2d4840e9
Ishan <ishansrivastava123@gmail.com>
2023-05-17 23:54:50 +0100

    random changes

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    // headers: ["Content-Type", "X-Auth-Token", "Origin", "Authorization"],
+    headers: ["Content-Type", "X-Auth-Token", "Origin", "Authorization"],
     credentials: true
 }));
 
